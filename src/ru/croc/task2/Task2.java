e059d0768633f523919a3a9d5cf962c8465ea6d4
dazhu <dazhuk2016@yandex.ru>
2022-10-24 21:40:27 +0300

    task2_fixed

--- src/ru/croc/task2/Task2.java
+++ src/ru/croc/task2/Task2.java
@@ -2,36 +2,19 @@ package ru.croc.task2;
 
 import java.util.Scanner;
 
+
 public class Task2 {
     public static void storageUnits(double sizeFile){
         int sizePrefix = 0;
-        while(sizeFile > 1024){
+        String[] units = {"B", "KB", "MB", "GB", "TB"};
+        while(sizeFile > 1024 && sizePrefix < 4){
             sizeFile/=1024;
             sizePrefix++;
         }
-        switch (sizePrefix){
-            case 0:
-                System.out.print(String.format("%.1f", sizeFile) + " B");
-                break;
-            case 1:
-                System.out.print(String.format("%.1f", sizeFile) + " KB");
-                break;
-            case 2:
-                System.out.print(String.format("%.1f", sizeFile) + " MB");
-                break;
-            case 3:
-                System.out.print(String.format("%.1f", sizeFile) + " GB");
-                break;
-            case 4:
-                System.out.print(String.format("%.1f", sizeFile) + " TB");
-                break;
-            case 5:
-                System.out.print(String.format("%.1f", sizeFile) + " PB");
-                break;
-            default:
-                System.out.print("File size is too large!");
-                break;
-        }
+        if (sizePrefix == 4)
+            System.out.print(String.format("%.1f ", sizeFile) + " " + units[sizePrefix]);
+        else
+            System.out.print(String.format("%.1f ", sizeFile) + units[sizePrefix]);
     }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
