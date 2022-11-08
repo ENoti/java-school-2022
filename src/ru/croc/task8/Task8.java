6d86a408f903e8b794e0023f759df184f3824645
dazhu <dazhuk2016@yandex.ru>
2022-11-09 02:29:28 +0300

    task8_second

--- src/ru/croc/task8/Task8.java
+++ src/ru/croc/task8/Task8.java
@@ -4,8 +4,8 @@ import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class Task8 {
-    public static void main(String args) throws Exception{
-        File file = new File(args);
+    public static void main(String[] args) throws Exception{
+        File file = new File(args[0]);
         try(Scanner sc = new Scanner(new FileInputStream(file))){
             int countWords = 0;
             while(sc.hasNext()){
