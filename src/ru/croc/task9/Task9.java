2b2ecc7d5a001230c7b0f36025784cf6af028658
dazhu <dazhuk2016@yandex.ru>
2022-11-16 14:50:29 +0300

    task9

--- src/ru/croc/task9/Task9.java
+++ src/ru/croc/task9/Task9.java
@@ -2,11 +2,9 @@ package ru.croc.task9;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Task9 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
         String enteredStr = args[0];
         String[] words = enteredStr.split("/");
         ArrayList<String> words_list = new ArrayList<>(List.of(words));
@@ -17,13 +15,18 @@ public class Task9 {
             words_list.remove(words_list.indexOf("..") - 1);
             words_list.remove("..");
             if(words_list.indexOf("..") == 0){
-                String set = words_list.set(words_list.indexOf(".."), "...");
+                words_list.set(words_list.indexOf(".."), "...");
+            }
+            if (words_list.indexOf("..") == 1 && words_list.indexOf("..") == 0){
+                words_list.set(words_list.indexOf("..") + 1, "...");
             }
         }
-        if(words_list.indexOf("...") == 0){
+        if(words_list.indexOf("...") == 0 && words_list.indexOf("...") == 1){
             words_list.set(words_list.indexOf("..."), "..");
         }
-        System.out.println(words_list);
-
+        System.out.print(words_list.get(0));
+        for (int i = 1; i < words_list.size(); i++){
+            System.out.print("/" + words_list.get(i));
+        }
     }
 }
